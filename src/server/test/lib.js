9ae89f201a8eaacb3f7a7eec8d9ec844da77f90c
Christo Fogelberg <cgf@syntilect.com>
2015-04-26 12:12:53 +0100

    server/test/lib.js - function to create it() test that checks per-file export tests are comprehensive

--- src/server/test/lib.js
+++ src/server/test/lib.js
@@ -1,11 +1,24 @@
+// Allow statements that are not assignments or function calls (e.g. should statements)
+/* jshint -W030 */
+
 'use strict';
 
 var _ = require('underscore');
 var glob = require('glob');
+var should = require('should');
 var sinon = require('sinon');
 
 var is_application_file = function(filename) {
   return !/.*_itest\.js/.test(filename) && !/.*_utest\.js/.test(filename);
+};
+
+/**
+ * Used by exports.create_test_to_check_exports so defined separately here
+ */
+var js_app_files_not_router_in_dir = function(dir) {
+  return glob.sync(dir + '/**/*.js').filter(is_application_file).filter(function(filename) {
+    return !/.*\/router.js/.test(filename);
+  });
 };
 
 module.exports = {
@@ -27,9 +40,7 @@ module.exports = {
    * @return {Array}      Array of String with the relative paths to the files in dir
    */
   js_app_files_not_router_in_dir: function(dir) {
-    return glob.sync(dir + '/**/*.js').filter(is_application_file).filter(function(filename) {
-      return !/.*\/router.js/.test(filename);
-    });
+    return js_app_files_not_router_in_dir(dir);
   },
 
   /**
@@ -39,6 +50,17 @@ module.exports = {
    */
   num_func_in_module: function(require_path) {
     return _.functions(require(require_path)).length;
+  },
+
+  /**
+   * Returns the number of non-functions exported by a module
+   * @param  {String} require_path The path to the module for require
+   * @return {Number}              The number of functions
+   */
+  num_nonfunc_in_module: function(require_path) {
+    var module = require(require_path);
+    var module_functions = _.functions(module);
+    return _.keys(module).filter(function(property) { return module_functions.indexOf(property) === -1; }).length;
   },
 
   /**
@@ -52,5 +74,34 @@ module.exports = {
   stub_then_done: function() {
     var mock_then_done_promise = { then: sinon.spy(function() { return { done: sinon.spy() }; }) };
     return sinon.spy(function() { return mock_then_done_promise; });
+  },
+
+  /**
+   * Returns a should it() test that validates the number of tests executed and stored in an object map versus the
+   * number of properties, functions (or using some other comparison) on the objects exported by files in a directory
+   * path. The purpose of this is to confirm that the tests which wrote values to the object map tested everything of
+   * the right type exported by the files in the directory path.
+   *
+   * @param  {[type]} description_string The description of the test to be created
+   * @param  {[type]} load_path          The directory path to load the files to check exports for
+   * @param  {[type]} export_type_string The string describing the type of check, e.g. 'method exports'
+   * @param  {[type]} comp_map           The object map keyed by file with the number of checks for each file checked
+   * @param  {[type]} comp_func          The comparison function taking a path to a file to compare against comp_map
+   * @return {[type]}                    A should it() test
+   */
+  create_test_to_check_exports: function(description_string, load_path, export_type_string, comp_map, comp_func) {
+    return it(description_string, function() {
+      var js_files = js_app_files_not_router_in_dir(load_path);
+      js_files.forEach(function(js_file) {
+        var req_path = js_file.replace(/\.\/src\/server\//, '').replace(/\.js$/, '');
+        should(comp_map[req_path]).not.be.type('undefined', req_path + ' ' + export_type_string + ' not checked');
+        if(comp_map[req_path]) {
+          var nf_tested = comp_map[req_path];
+          var nf_in_module = comp_func(req_path);
+          nf_tested.should.equal(nf_in_module,
+            req_path + ' only ' + nf_tested + ' of ' + nf_in_module + ' ' + export_type_string + ' checked');
+        }
+      });
+    });
   }
 };
