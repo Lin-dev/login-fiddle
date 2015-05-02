fe20f70f2bd5c639dd7f142186e76cfcde00077e
Christo Fogelberg <cgf@syntilect.com>
2015-05-02 19:04:57 +0100

    client/apps/user/signup/controller.js - stub signup impl using $ not Backbone models

--- src/client/js/apps/user/signup/controller.js
+++ src/client/js/apps/user/signup/controller.js
@@ -5,6 +5,18 @@ define(function(require) {
   var logger = PF.logger.get('root/js/apps/user/signup/controller');
 
   PF.module('UserApp.Signup', function(Signup, PF, Backbone, Marionette, $, _) {
+    var process_signup_response = function(data, textStatus, jqXhr) {
+      if(data.status === 'success') {
+        logger.debug('UserApp.Signup - process_signup_response -- signup status: ' + data.status);
+      }
+      else if(data.status === 'failure') {
+        logger.debug('UserApp.Signup - process_signup_response -- signup status: ' + data.status);
+      }
+      else {
+        logger.warn('UserApp.Signup - process_signup_response -- unknown status: ' + data.status);
+      }
+    };
+
     Signup.controller = {
       show_signup_form: function() {
         logger.trace('show_signup_form -- enter');
@@ -13,7 +25,7 @@ define(function(require) {
         view.on('login-clicked', function() { PF.trigger('user:login'); });
         view.on('home-clicked', function() { PF.trigger('home:show'); });
         view.on('local-signup-submitted', function(data) {
-          console.log('FORM SUBMITTED - need to use data in here as part of an API submission');
+          $.post('https://localhost:27974/api/user/signup', data, process_signup_response);
         });
         PF.region_main.show(view);
       }
