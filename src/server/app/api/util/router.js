9ca9c7f8e059d68e4f6fb2e93fb5d4375397ea6d
Christo Fogelberg <cgf@syntilect.com>
2015-05-02 18:41:19 +0100

    app/api/util/router_impl - router.put --> router.get bug fix

--- src/server/app/api/util/router.js
+++ src/server/app/api/util/router.js
@@ -5,6 +5,6 @@ var router_impl = require('app/api/util/router_impl');
 var express = require('express');
 var router = new express.Router();
 router.get('/success', router_impl.success);
-router.put('/failure', router_impl.failure);
+router.get('/failure', router_impl.failure);
 
 module.exports = router;
