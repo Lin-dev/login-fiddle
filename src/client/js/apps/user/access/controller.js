323575d94537e12da7625ba242c3e6455cade1a0
Christo Fogelberg <cgf@syntilect.com>
2015-05-08 20:16:41 +0100

    client/js/apps/user/access/controller.js - in progress controller to show single signup/local login form

--- src/client/js/apps/user/access/controller.js
+++ src/client/js/apps/user/access/controller.js
@@ -5,43 +5,74 @@ define(function(require) {
   var logger = PF.logger.get('root/js/apps/user/access/controller');
 
   PF.module('UserApp.Access', function(Access, PF, Backbone, Marionette, $, _) {
-    Access.controller = {
-      show_access_form: function() {
-        logger.trace('show_access_form -- enter');
-        var Views = require('js/apps/user/access/views');
-        // Model is needed in view so that view can be updated following if the post response is a failure
-        var view = new Views.AccessForm({ model: new PF.Entities.PFClientOnlyModel() });
-        view.on('home-clicked', function() { PF.trigger('home:show'); });
-        view.on('local-access-submitted', function(submitted_data) {
-
-
-          require('js/apps/user/entities');
-          // UserLocalAccess just for validation (passport redirect mucks up Backbone model sync)
-          var new_user = new PF.UserApp.Entities.UserLocalAccess(submitted_data);
-          var validation_errors = new_user.validate(new_user.attributes);
-          if(_.isEmpty(validation_errors)) {
-            logger.debug('User validation passed - no errors');
-            $.post('/api/user/access/local', submitted_data, function(resp_data, textStatus, jqXhr) {
-              if(resp_data.status === 'success') {
-                logger.debug('UserApp.Access - local_access post response -- access succeess, redirecting to profile');
-                PF.trigger('user:profile');
-              }
-              else if(resp_data.status === 'failure') { // duplicate email, server side validation error or ?
-                logger.debug('UserApp.Access - local_access post response -- access failure: ' + resp_data.message);
-                view.model.set({ message: resp_data.message });
-              }
-              else {
-                logger.error('UserApp.Access - local_access post response -- unknown status: ' + resp_data.status +
-                  ' (message: ' + resp_data.message + ')');
-              }
-            });
+    function process_local_login(form_data, access_view) {
+      // UserLocalAccess just for validation (passport redirect mucks up Backbone model sync)
+      var ula = new PF.UserApp.Entities.UserLocalAccess(form_data);
+      var validation_errors = ula.validate(ula.attributes);
+      if(_.isEmpty(validation_errors)) {
+        logger.debug('private.process_local_login -- access form validation passed: ' + JSON.stringify(form_data));
+        $.post('/api/user/access/local/login', form_data, function(resp_data, textStatus, jqXhr) {
+          if(resp_data.status === 'success') {
+            logger.debug('private.process_local_login - /access/local/login response -- ' +
+              'succeess, redirecting to profile');
+            PF.trigger('user:profile');
+          }
+          else if(resp_data.status === 'failure') {
+            logger.debug('private.process_local_login - /access/local/login response -- ' +
+              'login failure: ' + resp_data.message);
+            logger.warn(JSON.stringify(resp_data));
+            access_view.model.set({ message: resp_data.message });
           }
           else {
-            logger.debug('User validation failed: ' + JSON.stringify(validation_errors));
-            view.show_validation_errors(validation_errors);
+            logger.error('private.process_local_login - /access/local/login response -- ' +
+              'unknown status: ' + resp_data.status + ' (message: ' + resp_data.message + ')');
+            access_view.model.set({ message: resp_data.message });
           }
         });
-        PF.region_main.show(view);
+      }
+      else {
+        logger.debug('private.process_local_login -- user validation failed: ' + JSON.stringify(validation_errors));
+        access_view.show_validation_errors(validation_errors);
+      }
+    }
+
+    function process_local_signup(form_data) {
+      logger.debug('private.process_local_signup: ' + JSON.stringify(form_data));
+      //
+      //
+      // TODO
+      //
+      //
+      //
+    }
+
+    Access.controller = {
+      show_access_form: function() {
+        logger.trace('UserApp.Access.controller.show_access_form -- enter');
+        var Views = require('js/apps/user/access/views');
+        // Model is needed in view so that view can be updated following if the post response is a failure
+        var access_view = new Views.AccessForm({ model: new PF.Entities.PFClientOnlyModel() });
+        access_view.on('home-clicked', function() { PF.trigger('home:show'); });
+        access_view.on('local-access-submitted', function local_access_submitted(form_data) {
+          require('js/apps/user/entities');
+
+          if(form_data.has_pw_flag === 'true') { // attempt a login using email / password
+            process_local_login(form_data, access_view);
+          }
+          else if(form_data.has_pw_flag === 'false') { // show the signup form
+            var uls = new PF.UserApp.Entities.UserLocalSignup({ email: form_data.email });
+            var signup_view = new Views.SignupForm({ model: uls });
+            signup_view.on('home-clicked', function() { PF.trigger('home:show'); });
+            signup_view.on('login-clicked', function() { PF.trigger('user:login'); });
+            signup_view.on('local-signup-submitted', function(form_data) { process_local_signup(form_data); });
+            PF.region_main.show(signup_view);
+          }
+          else {
+            logger.error('UserApp.Access.controller.show_access_form - local-access-submitted callback -- ' +
+              'unknown has_pw_flag: ' + form_data.has_pw_flag);
+          }
+        });
+        PF.region_main.show(access_view);
       }
     };
   });
