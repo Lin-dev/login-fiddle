8769a7b7edc1682d3d50d52c09e7259aa4abb705
Christo Fogelberg <cgf@syntilect.com>
2015-05-12 08:37:02 +0100

    server/app/server.js - more logical middleware ordering

--- src/server/app/server.js
+++ src/server/app/server.js
@@ -47,8 +47,7 @@ https_server.listen(server_config.https_port, function() {
 function configure_app_middleware(app) {
   var logger_config = require('app/config/logger');
 
-  app.use(compression());
-  app.use(serve_favicon(path.join(server_config.client_root, 'assets', 'images', 'favicons', 'favicon.ico')));
+  // (1) Logging
   app.use(log4js.connectLogger(logger_module.get_log4js('connect-appender'), {
     level: 'auto',
     layout: 'basic',
@@ -56,6 +55,24 @@ function configure_app_middleware(app) {
     format: logger_config.express_format,
     tokens: logger_config.custom_tokens
   }));
+
+  // (2) Parse request
+  app.use(body_parser.json());
+  app.use(body_parser.urlencoded({ extended: false }));
+  app.use(method_override('X-HTTP-Method'));          // Microsoft
+  app.use(method_override('X-HTTP-Method-Override')); // Google/GData
+  app.use(method_override('X-Method-Override'));      // IBM
+
+  // (3) Serve static routes
+  app.use(compression());
+  app.use(serve_favicon(path.join(server_config.client_root, 'assets', 'images', 'favicons', 'favicon.ico')));
+  app.use('/assets', express.static(path.join(server_config.client_root, 'assets'),
+    { maxAge: server_config.static_max_age }));
+  app.use('/bower_components', express.static(path.join(server_config.client_root, 'bower_components'),
+    { maxAge: server_config.static_max_age }));
+  app.use('/js', express.static(path.join(server_config.client_root, 'js'), { maxAge: server_config.static_max_age }));
+
+  // (4) Cookies and sessions
   app.use(session({
     cookie: {
       maxAge: server_config.session.cookie.maxAge,
@@ -83,28 +100,24 @@ function configure_app_middleware(app) {
     next();
   });
   app.use(connect_flash());
-  app.use(body_parser.json());
-  app.use(body_parser.urlencoded({ extended: false }));
-  app.use(method_override('X-HTTP-Method'));          // Microsoft
-  app.use(method_override('X-HTTP-Method-Override')); // Google/GData
-  app.use(method_override('X-Method-Override'));      // IBM
   app.use(function(req, res, next) { // TODO - temp init of session, factor this out somewhere
     if(!req.session.start) {
       req.session.start = new Date();
     }
     next();
   });
+
+  // (5) Server dynamic routes
   app.use('/api', require('app/api/router'));
-  app.use('/assets', express.static(path.join(server_config.client_root, 'assets'),
-    { maxAge: server_config.static_max_age }));
-  app.use('/bower_components', express.static(path.join(server_config.client_root, 'bower_components'),
-    { maxAge: server_config.static_max_age }));
-  app.use('/js', express.static(path.join(server_config.client_root, 'js'), { maxAge: server_config.static_max_age }));
+
+  // (6) Fall back
   app.use(function(req, res, next) { // Fall back to always sending index.html
     logger.debug('client request ' + req.originalUrl + ' (route: ' + JSON.stringify(req.route) +
       ') has fallen through to index.html catch');
     res.sendFile(path.join(server_config.client_root, 'index.html'));
   });
+
+  // (7) Error handling
   // TODO not appropriate for a prod site, see SES / http://calv.info/node-and-express-tips/ for another approach:
   app.use(errorhandler());
 }
