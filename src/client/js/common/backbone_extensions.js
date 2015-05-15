c3139b41e847ccf010c9bc3ef9d98775ffd88183
Christo Fogelberg <cgf@syntilect.com>
2015-05-15 06:39:23 +0100

    client/js/common/backbone_extensions.js - removed unnecessary IIFE

--- src/client/js/common/backbone_extensions.js
+++ src/client/js/common/backbone_extensions.js
@@ -1,3 +1,5 @@
+// NO LOGGING IN THIS FILE
+
 /**
  * This require does two things:
  * - Extends Backbone.Model, .View, .Router and .Collection to use named constructors for more informative debugging
@@ -16,52 +18,50 @@ define(function(require) {
 
   var AppObj = require('js/app/obj');
   AppObj.module('Entities', function(Entities, AppObj, Backbone, Marionette, $, _) {
-    (function () {
-      function createNamedConstructor(name, constructor) {
-        // Explicitly allow use of Function constructor here
-        /* jshint -W054 */
-        var fn = new Function('constructor', 'return function ' + name + '() {\n' +
-          '  // wrapper function created dynamically for "' + name + '" constructor to allow instances to be\n' +
-          '  // identified in the debugger\n' +
-          '  constructor.apply(this, arguments);\n' +
-          '};');
-        return fn(constructor);
+    function createNamedConstructor(name, constructor) {
+      // Explicitly allow use of Function constructor here
+      /* jshint -W054 */
+      var fn = new Function('constructor', 'return function ' + name + '() {\n' +
+        '  // wrapper function created dynamically for "' + name + '" constructor to allow instances to be\n' +
+        '  // identified in the debugger\n' +
+        '  constructor.apply(this, arguments);\n' +
+        '};');
+      return fn(constructor);
+    }
+
+    var original_extend = Backbone.View.extend; // Model, Collection, Router and View shared the same extend function
+    var name_property = '__name';
+
+    var newExtend = function(protoProps, classProps) {
+      if (protoProps && protoProps.hasOwnProperty(name_property)) {
+        var name = protoProps[name_property]; // NB: this does not check that name_property is a valid identifier
+        // wrap constructor from protoProps if supplied or 'this' (the function we are extending)
+        var constructor = protoProps.hasOwnProperty('constructor') ? protoProps.constructor : this;
+        protoProps = _.extend(protoProps, {
+          constructor: createNamedConstructor(name, constructor)
+        });
       }
+      return original_extend.call(this, protoProps, classProps);
+    };
 
-      var original_extend = Backbone.View.extend; // Model, Collection, Router and View shared the same extend function
-      var name_property = '__name';
+    Backbone.Model.extend = Backbone.Collection.extend = Backbone.Router.extend = Backbone.View.extend = newExtend;
 
-      var newExtend = function(protoProps, classProps) {
-        if (protoProps && protoProps.hasOwnProperty(name_property)) {
-          var name = protoProps[name_property]; // NB: this does not check that name_property is a valid identifier
-          // wrap constructor from protoProps if supplied or 'this' (the function we are extending)
-          var constructor = protoProps.hasOwnProperty('constructor') ? protoProps.constructor : this;
-          protoProps = _.extend(protoProps, {
-            constructor: createNamedConstructor(name, constructor)
-          });
-        }
-        return original_extend.call(this, protoProps, classProps);
-      };
+    Backbone.Model.prototype[name_property] = 'Model';
+    Backbone.Collection.prototype[name_property] = 'Collection';
+    Backbone.Router.prototype[name_property] = 'Router';
+    Backbone.View.prototype[name_property] = 'View';
 
-      Backbone.Model.prototype[name_property] = 'Model';
-      Backbone.Collection.prototype[name_property] = 'Collection';
-      Backbone.Router.prototype[name_property] = 'Router';
-      Backbone.View.prototype[name_property] = 'View';
+    Backbone.Model.prototype.toString = function() {
+      return this[name_property] + '(cid: ' + this.cid + ', attr: ' + JSON.stringify(this.attributes) + ')';
+    };
 
-      Backbone.Model.prototype.toString = function() {
-        return this[name_property] + '(cid: ' + this.cid + ', attr: ' + JSON.stringify(this.attributes) + ')';
-      };
+    Backbone.Collection.prototype.toString = function() {
+      return this[name_property] + '(models: ' + JSON.stringify(this.models) + ')';
+    };
 
-      Backbone.Collection.prototype.toString = function() {
-        return this[name_property] + '(models: ' + JSON.stringify(this.models) + ')';
-      };
-
-      Backbone.View.prototype.toString = function() {
-        return this[name_property] + '(cid: ' + this.cid + ', outerHTML: ' + this.$el.prop('outerHTML') + ')';
-      };
-
-      Backbone.Model.extend = Backbone.Collection.extend = Backbone.Router.extend = Backbone.View.extend = newExtend;
-    })();
+    Backbone.View.prototype.toString = function() {
+      return this[name_property] + '(cid: ' + this.cid + ', outerHTML: ' + this.$el.prop('outerHTML') + ')';
+    };
   });
 
   return undefined;
