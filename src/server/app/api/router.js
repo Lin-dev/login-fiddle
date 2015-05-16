efa2b433943a78a34f6558aa1b37c286da1ba7b1
Christo Fogelberg <cgf@syntilect.com>
2015-05-16 13:37:09 +0100

    server/app/api/router.js - dynamically load and map API module into app router

--- src/server/app/api/router.js
+++ src/server/app/api/router.js
@@ -2,12 +2,28 @@
 
 // No logger defined or used because file code is just Express glue
 
+var fs = require('fs');
+
 var express = require('express');
+var logger_module = require('app/util/logger');
+var logger = logger_module.get('app/api/router');
 var router = new express.Router();
 
-router.use('/entry', require('app/api/entry/router'));
-router.use('/session', require('app/api/session/router'));
-router.use('/user', require('app/api/user/router'));
-router.use('/util', require('app/api/util/router'));
+var all_possible_api_modules = fs.readdirSync(__dirname);
+all_possible_api_modules.forEach(function(possible_module_name) {
+  try {
+    var require_path = 'app/api/' + possible_module_name + '/router';
+    var possible_router = require(require_path);
+
+    if(possible_router !== undefined) { // then possible module is a real module and possible_router is a real router
+      var server_requests_path = '/' + possible_module_name;
+      logger.debug('API module mapped to ' + server_requests_path + ' - ' + require_path);
+      router.use(server_requests_path, possible_router);
+    }
+  }
+  catch(e) {
+    logger.debug('API module not present at ' + require_path);
+  }
+});
 
 module.exports = router;
