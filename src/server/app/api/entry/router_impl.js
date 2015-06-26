2a3eb59d94e00c3aebbfc8e1510454ac62f89c72
Christo Fogelberg <cgf@syntilect.com>
2015-06-26 20:24:39 +0100

    server/app/api/entry/router_impl.js - wrap PR find and findAll calls in q promises to get fail method

--- src/server/app/api/entry/router_impl.js
+++ src/server/app/api/entry/router_impl.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var _ = require('underscore');
+var q = require('q');
 
 var logger_module = require('app/util/logger');
 var logger = logger_module.get('app/api/entry/router_impl');
@@ -12,10 +13,10 @@ module.exports = {
   get_entries: function get_entries(pr, req, res, next) {
     logger.trace('exports.get_entries -- enter');
     if(req.params.tag_string) {
-      pr.pr.entry.tag.find({
+      q(pr.pr.entry.tag.find({
         where: { value: req.params.tag_string },
         include: [{ model: pr.pr.entry.entry, include: [pr.pr.entry.tag] }]
-      })
+      }))
       .then(function(tag) {
         res.status(200).send(_.map(tag.entries, function(entry) {
           return entry.get({ plain: true});
@@ -28,10 +29,10 @@ module.exports = {
       .done();
     }
     else {
-      pr.pr.entry.entry.findAll({ include: [{model: pr.pr.entry.tag }]})
+      q(pr.pr.entry.entry.findAll({ include: [{model: pr.pr.entry.tag }]})
       .then(function(entry_instances) {
         res.status(200).send(_.map(entry_instances, function(entry) { return entry.get({ plain: true }); }));
-      })
+      }))
       .fail(function(err) {
         logger.error('exports.get_entries -- promise fail: ' + err);
         res.status(500).end();
@@ -45,7 +46,7 @@ module.exports = {
    */
   get_tags: function get_tags(pr, req, res, next) {
     logger.trace('exports.get_tags -- enter');
-    pr.pr.entry.tag.findAll()
+    q(pr.pr.entry.tag.findAll())
     .then(function(tag_instances) {
       res.status(200).send(_.map(tag_instances, function(tag) { return tag.get({ plain: true}); }));
     })
